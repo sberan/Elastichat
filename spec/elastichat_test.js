1903f0ad44df43cb60229ac3faf474be1e203eea
Sam Beran <sberan@gmail.com>
2012-01-19 15:18:50 -0600

    add some tests

--- spec/elastichat_test.js
+++ spec/elastichat_test.js
@@ -1,6 +1,7 @@
-var Stubble = require('stubble');
-var events = require('events');
-var assert = require('assert');
+var Stubble = require('stubble'),
+    events = require('events'),
+    assert = require('assert'),
+    _ = require('underscore');
 
 var ircClient = new events.EventEmitter();
 ircClient.join = function(channel){
@@ -13,9 +14,11 @@ var irc = {
   }
 }
 
+var docs = [];
+var execs = 0
 var es = function() {
-  this.index = function(){ return this }
-  this.exec = function(){}
+  this.index = function(index, type, doc){docs.push(doc); return this; }
+  this.exec = function(){execs++;}
 }
 
 var stub = new Stubble({irc: irc, elasticsearchclient: es});
@@ -35,10 +38,17 @@ var Elastichat = stub.require(__dirname + "/../lib/elastichat.js"),
     });
 
 ircClient.emit("registered");
-
 ircClient.emit("join", "#foo");
+ircClient.emit("message","s_b", "#foo", "yo!");
 
 setTimeout(function(){
-  assert(true);
+  var channel = docs.shift();
+  assert.equal(channel.id, "##javascript");
+  channel = docs.shift();
+  assert.equal(channel.channel, "#foo");
+  var message = docs.shift();
+  assert.equal(message.sender, "s_b");
+  assert.equal(message.channel, "#foo");
+  assert.equal(message.message, "yo!");
 
 }, 1000);
